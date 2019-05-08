df0b776a613c636c18a66c996c3ce6190094d10a
Natalia Filippova <fnd.natalia@gmail.com>
2019-05-08 22:05:26 +0530

    Repalced Paper.js with konva and react-spring; made the circles bigger and added animation

--- src/App.js
+++ src/App.js
@@ -1,64 +1,42 @@
 import React, { Component } from 'react';
-import {
-  View,
-  Layer,
-  Group,
-  Path,
-  Circle,
-  Ellipse,
-  Rectangle,
-  PointText,
-  Tool,
-} from 'react-paper-bindings';
-import styled from 'styled-components';
+import { Stage, Layer, Circle, Text } from 'react-konva';
+import Konva from 'konva';
+import styled, { keyframes } from 'styled-components';
 import uuidv4 from 'uuid/v4';
+import {Spring} from 'react-spring/renderprops';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      mounted: false,
       circles: [],
     }
-
-    this._box = null
-    this._request = null
-  }
-
-  resizeWindow() {
-    this.forceUpdate()
-  }
-
-  resizePaper() {
-    this._request = null
   }
 
   componentDidMount() {
-    this.setState({ mounted: true })
-    window.addEventListener('resize', () => this.resizeWindow())
-    window.addEventListener('keydown', (e) => this.onKeyDown(e))
+    window.addEventListener('keydown', () => this.onKeyDown())
   }
+
 
   componentWillUnmount() {
-    window.removeEventListener('resize', () => this.resizeWindow())
   }
 
-  newCoordinate() {}
 
   onKeyDown(event) {
     const array = this.state.circles;
 
     const newId = uuidv4();
     const newCircle = {
-      x: Math.floor(Math.random() * (this.box().width + 1)),
-      y: Math.floor(Math.random() * (this.box().height + 1)),
-      color: this.newColor(),
+      x: Math.floor(Math.random() * (window.innerWidth + 1)),
+      y: Math.floor(Math.random() * (window.innerHeight + 1)),
+      color: Konva.Util.getRandomColor(),
+      duration: ((Math.random() * 1.7) + 0.5) * 1000,
       id: newId,
     }
 
-    array.push(newCircle)
-    this.setState({ circles: array })
+    array.push(newCircle);
+    this.setState({ circles: array });
 
     setTimeout(() => {
       if (newCircle.id === newId) {
@@ -66,50 +44,48 @@ class App extends Component {
         const updateArr = array.filter(x => x!== newCircle);
         this.setState({ circles: updateArr })
       }
-    }, 2000)
-  }
-
-  randomColorValue() {
-    return Math.floor(Math.random() * (255 + 1));
-  }
-
-  newColor() {
-    const color = {r: this.randomColorValue(), g: this.randomColorValue(), b: this.randomColorValue()};
-    return `rgb(${color.r}, ${color.g}, ${color.b})`
-  }
-
-  box() {
-    return this._box && this._box.getBoundingClientRect()
+    }, newCircle.duration)
   }
 
   render() {
-    const { mounted } = this.state
-
     return (
-      <AppContainer ref={ref => this._box = ref}>
-      {mounted &&
-        <PaperContainer
-            width={this.box().width}
-            height={this.box().height}>
-          <Layer>
-            <Layer>
-              {this.state.circles.map(circle => (
-                <Circle center={[circle.x, circle.y]} radius={8} fillColor={circle.color} />
-              ))}
-            </Layer>
-          </Layer>
-        </PaperContainer>
-      }
-      </AppContainer>
+      <StyledStage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          {this.state.circles.map(circle => (
+            <Spring key={circle.id} config={{ duration: circle.duration }} from={{ radius: 100 }} to={{ radius: 0 }}>
+              {props => (
+                [
+                  <Circle radius={props.radius} x={circle.x} y={circle.y} fill={circle.color} />,
+                  <Circle radius={props.radius + 5} x={circle.x} y={circle.y} stroke={'white'} />,
+                  <Circle radius={props.radius + 15} x={circle.x} y={circle.y} stroke={circle.color} />,
+                  <Circle radius={props.radius + 20} x={circle.x} y={circle.y} stroke={'white'} />
+                ]
+              )}
+            </Spring>
+          ))}
+        </Layer>
+      </StyledStage>
     );
   }
 }
 
-const PaperContainer = styled(View)`
+const StyledStage = styled(Stage)`
   background: black;
 `
 const AppContainer = styled.div`
   height: 100vh;
   width: 100vw;
+`
+const zoom = keyframes`
+    0% {
+        transform: scale(1,1);
+    }
+    100% {
+        transform: scale(0,0);
+    }
+`
+const StyledCircle = styled.circle`
+  /* transform: scale(2); */
+  transition: transform 3s;
 `
 export default App;
